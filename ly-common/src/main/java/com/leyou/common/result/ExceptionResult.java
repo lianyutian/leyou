b14dd7f2b3920fae72dc7fd151a4c0c82517dac1
lianyutian <1436159527@qq.com>
2020-01-07 22:42:06 +0800

    添加公用方法，通用异常处理

---
+++ ly-common/src/main/java/com/leyou/common/result/ExceptionResult.java
@@ -0,0 +1,20 @@
+package com.leyou.common.result;
+
+import com.leyou.common.enums.ExceptionEnum;
+import lombok.Data;
+
+@Data
+public class ExceptionResult {
+    // 状态码
+    private int status;
+    // 错误信息
+    private String message;
+    // 时间戳
+    private Long timestamp;
+
+    public ExceptionResult(ExceptionEnum em) {
+        this.status = em.getCode();
+        this.message = em.getMsg();
+        this.timestamp = System.currentTimeMillis();
+    }
+}
